02f335f8eafadf5d9e89f359a807802030e10b81
youssef336 <youssefbelal2004@gmail.com>
2025-02-23 18:35:14 +0200

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constant.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/Note_view.dart';
 import 'package:note_app/views/edit_note_view.dart';
@@ -19,15 +21,19 @@ class Notes_app extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: NotePage.id,
-      routes: {
-        NotePage.id: (context) => const NotePage(),
-        EditNoteView.id: (context) => const EditNoteView(),
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      home: NotePage(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
+
+      child: MaterialApp(
+        initialRoute: NotePage.id,
+        routes: {
+          NotePage.id: (context) => const NotePage(),
+          EditNoteView.id: (context) => const EditNoteView(),
+        },
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: NotePage(),
+      ),
     );
   }
 }
