b86b25550d8ff9125d18b434781e103a350c92b2
youssef336 <youssefbelal2004@gmail.com>
2025-02-28 13:15:33 +0200

    create feach note method

--- lib/views/Note_view.dart
+++ lib/views/Note_view.dart
@@ -2,6 +2,8 @@
 // ignore_for_file: file_names, duplicate_ignore
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/views/widgets/Notes_view_body.dart';
 import 'package:note_app/views/widgets/add_note_buttom_sheet.dart';
 
@@ -10,21 +12,24 @@ class NotePage extends StatelessWidget {
   static String id = 'NotePage';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.cyan,
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.cyan,
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: NotesView_Body(),
       ),
-      body: NotesView_Body(),
     );
   }
 }
